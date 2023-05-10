7795ac8a1a020c701e72e0b08a8316fe0f1bc6db
ADEK iMac Pro 1 <adek@ADEKs-iMac-Pro.local>
2023-05-10 14:37:00 +0400

    Description: successful connection with IRC client (LimeChat). uppon connecting, client successfully send message to server with nickname and password. Messages are also received from the client using poll().
    Outcome:
    We can now move into creating proper classes for server and each client connection.
    We can now start the message parser to validate the password.
    We can now work on sending messages from server back to the client.

--- src/socket.cpp
+++ src/socket.cpp
@@ -6,21 +6,34 @@
 /*   By: kamin <kamin@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:39:21 by kamin             #+#    #+#             */
-/*   Updated: 2023/05/09 15:49:00 by kamin            ###   ########.fr       */
+/*   Updated: 2023/05/10 14:33:44 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <fstream>
 #include <cstdint>
 #include <iostream>
+#include <poll.h>
+
+// ON CONNECTION THE BELOW MESSAGE IS SENT TO THE SO_RESOLVER_SIGNATURE
+// PASS 123123
+
+// NICK asd
+
+// USER asd 0 * asd
 
 int main(int ac, char  **av)
 {
     int d_socket;
     struct sockaddr_in address;
+    struct pollfd pfds[1];
+    
+
+
 
     d_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (d_socket == -1)
@@ -39,13 +52,38 @@ int main(int ac, char  **av)
             else
             {
                 int addrlen = sizeof(address);
+                std::cout << "Waiting for connection ..." << std::endl;
                 int new_socket = accept(d_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen);
                 if (new_socket >= 0)
                 {
-                    printf("Main Socket FD: %d\nconnect FD: %d\n",d_socket, new_socket);
-                    char buffer[1024] = { 0 };
-                    int val = recv(new_socket, buffer, 1024, MSG_WAITALL);
-                    printf("%s\n", buffer);
+                    struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&address;
+                    struct in_addr ipAddr = pV4Addr->sin_addr;
+                    printf("New Connection From: %s\n",inet_ntoa(ipAddr));
+                    pfds[0].fd = new_socket;
+                    pfds[0].events = POLLIN;
+                    
+                    // while (1)
+                    // {
+                        pfds[0].fd = new_socket;
+                        pfds[0].events = POLLIN;
+                            char buffer[1024] = { 0 };
+                        while(poll(pfds, 1, 1000) != -1) { /* error handling elided */
+                            // std::cout << "waiting for message..." << std::endl;
+                            if(pfds[0].revents & POLLIN) {
+                                int val = recv(new_socket, buffer, 1024, MSG_DONTWAIT);
+                                printf("%s\n", buffer);
+                                memset(buffer, 0, 1024);
+                            }
+                            if(pfds[0].revents & (POLLERR | POLLHUP)) {
+                                printf("Connection From: %s CLOSED!\n",inet_ntoa(ipAddr));
+                                pfds[0].fd = -1;
+                                exit(0);
+                            }
+                            // std::cout << "message recieved..." << std::endl;
+                        }
+                    // }
+                    
+                    
                 }
                 else
                     std::cout << "Failed to accept connection" << std::endl;
