c80605900f28d35ade34d19c4aa818eca77ae4f2
ADEK iMac Pro 1 <adek@ADEKs-iMac-Pro.local>
2023-05-11 17:10:23 +0400

    Description: - fixed polling to do its job properly. - added multi-connection support. - sending message from server -> client is working **1 - made all sockets non-blocking using fnctl
    **1: only 1 send command is working, the rest of the sends are not going through

--- src/socket.cpp
+++ src/socket.cpp
@@ -6,7 +6,7 @@
 /*   By: kamin <kamin@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:39:21 by kamin             #+#    #+#             */
-/*   Updated: 2023/05/10 14:33:44 by kamin            ###   ########.fr       */
+/*   Updated: 2023/05/11 17:06:10 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <cstdint>
 #include <iostream>
 #include <poll.h>
+#include <fcntl.h>
 
 // ON CONNECTION THE BELOW MESSAGE IS SENT TO THE SO_RESOLVER_SIGNATURE
 // PASS 123123
@@ -29,18 +30,15 @@
 int main(int ac, char  **av)
 {
     int d_socket;
+    int send_err;
     struct sockaddr_in address;
-    struct pollfd pfds[1];
-    
-
-
-
+    struct pollfd pfds[2];
     d_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (d_socket == -1)
+
+    if (d_socket == -1 && !fcntl(d_socket, F_SETFL, O_NONBLOCK))
         std::cout << "Failed to create socket" << std::endl;
     else
     {
-        std::cout << "Succeeded to create socket" << std::endl;
         std::cout << "Binding PORT ..." << std::endl;
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY;
@@ -53,40 +51,42 @@ int main(int ac, char  **av)
             {
                 int addrlen = sizeof(address);
                 std::cout << "Waiting for connection ..." << std::endl;
-                int new_socket = accept(d_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                if (new_socket >= 0)
+                pfds[0].fd = d_socket;
+                pfds[0].events = POLLIN;
+                pfds[1].fd = d_socket;
+                pfds[1].events = POLLIN;
+                int new_socket = 0;
+                while (poll(pfds, 2, 1000) != -1)
                 {
-                    struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&address;
-                    struct in_addr ipAddr = pV4Addr->sin_addr;
-                    printf("New Connection From: %s\n",inet_ntoa(ipAddr));
-                    pfds[0].fd = new_socket;
-                    pfds[0].events = POLLIN;
-                    
-                    // while (1)
-                    // {
-                        pfds[0].fd = new_socket;
-                        pfds[0].events = POLLIN;
-                            char buffer[1024] = { 0 };
-                        while(poll(pfds, 1, 1000) != -1) { /* error handling elided */
-                            // std::cout << "waiting for message..." << std::endl;
-                            if(pfds[0].revents & POLLIN) {
-                                int val = recv(new_socket, buffer, 1024, MSG_DONTWAIT);
-                                printf("%s\n", buffer);
-                                memset(buffer, 0, 1024);
-                            }
-                            if(pfds[0].revents & (POLLERR | POLLHUP)) {
-                                printf("Connection From: %s CLOSED!\n",inet_ntoa(ipAddr));
-                                pfds[0].fd = -1;
-                                exit(0);
-                            }
-                            // std::cout << "message recieved..." << std::endl;
+                    if (new_socket == 0)
+                    {
+                        new_socket = accept(d_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+                        struct sockaddr_in* pV4Addr = (struct sockaddr_in*)&address;
+                        struct in_addr ipAddr = pV4Addr->sin_addr;
+                        printf("New Connection From: %s\n",inet_ntoa(ipAddr));
+                        send_err = send(new_socket, ":kooki JOIN #kk\r\n", 18,0x80);
+                        pfds[1].fd = new_socket;
+                        pfds[1].events = POLLIN;
+                    }
+                    if (new_socket > 0)
+                    {
+                        char buffer[1024] = { 0 };
+                        if(pfds[1].revents & POLLIN) {
+                            send_err = send(new_socket, ":127.0.0.1 001 kooki :123123you there?\r\n", 41,0x80);
+                            int val = recv(new_socket, buffer, 1024, MSG_DONTWAIT);
+                            printf("%s\n", buffer);
+                            send_err = send(new_socket, ":127.0.0.1 001 kooki :you there?\r\n", 35,0x80);
+                            memset(buffer, 0, 1024);
                         }
-                    // }
-                    
-                    
+                        if(pfds[1].revents & (POLLERR | POLLHUP)) {
+                            printf("Connection CLOSED!\n");
+                            pfds[1].fd = -1;
+                            exit(0);
+                        }
+                    }
+                    else
+                        std::cout << "Failed to accept connection" << std::endl;
                 }
-                else
-                    std::cout << "Failed to accept connection" << std::endl;
             }
         }
         else
